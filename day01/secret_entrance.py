cb4765e46f95a13c4d9da2c1f75a83abeb6683a5
Walt Mankowski <waltman@pobox.com>
2025-12-01 05:41:10 -0500

    updated to solve 1.2

--- day01/secret_entrance.py
+++ day01/secret_entrance.py
@@ -1,7 +1,9 @@
 from sys import argv
 
 part1 = 0
+part2 = 0
 pos = 50
+pos2 = 50
 with open(argv[1]) as f:
     for line in f:
         direction = 1 if line[0] == 'R' else -1
@@ -10,4 +12,10 @@ with open(argv[1]) as f:
         if pos == 0:
             part1 += 1
 
+        for _ in range(cnt):
+            pos2 = (pos2 + direction) % 100
+            if pos2 == 0:
+                part2 += 1
+
 print('Part 1:', part1)
+print('Part 2:', part2)
