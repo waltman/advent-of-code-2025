8bae95987de413dd4e1879373e80fe202766c7c4
Walt Mankowski <waltman@pobox.com>
2025-12-01 11:21:35 -0500

    updated python solution to remove loop in part 2

--- day01/secret_entrance.py
+++ day01/secret_entrance.py
@@ -7,15 +7,22 @@ pos2 = 50
 with open(argv[1]) as f:
     for line in f:
         direction = 1 if line[0] == 'R' else -1
-        cnt = int(line[1:])
-        pos = (pos + (cnt * direction)) % 100
+        cnt = int(line[1:]) * direction
+        pos = (pos + cnt) % 100
         if pos == 0:
             part1 += 1
 
-        for _ in range(cnt):
-            pos2 = (pos2 + direction) % 100
-            if pos2 == 0:
-                part2 += 1
+        on_zero = pos2 == 0
+        pos2 += cnt
+        
+        # did we cross 0 going backwards?
+        if not on_zero and pos2 <= 0:
+            part2 += 1
+
+        # how many more times did we spin past 0?
+        part2 += abs(pos2) // 100
+        
+        pos2 %= 100
 
 print('Part 1:', part1)
 print('Part 2:', part2)
