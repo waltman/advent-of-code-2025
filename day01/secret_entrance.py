b85be7944e48599bea53a67af69c8caef70cbf6a
Walt Mankowski <waltman@pobox.com>
2025-12-01 05:37:05 -0500

    python code for 1.1

---
+++ day01/secret_entrance.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+part1 = 0
+pos = 50
+with open(argv[1]) as f:
+    for line in f:
+        direction = 1 if line[0] == 'R' else -1
+        cnt = int(line[1:])
+        pos = (pos + (cnt * direction)) % 100
+        if pos == 0:
+            part1 += 1
+
+print('Part 1:', part1)
