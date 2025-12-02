259c6c99d4331b7ade3a424534c00cb17bdb1869
Walt Mankowski <waltman@pobox.com>
2025-12-02 07:12:58 -0500

    updated for 2.2
    Not as fast as I'd like (about 4 1/2 seconds) but I'll take it.

--- day02/gift_shop.py
+++ day02/gift_shop.py
@@ -1,20 +1,31 @@
 from sys import argv
 
-def is_invalid(id):
-    size = len(id) // 2
-    return id[:size] == id[size:]
+def is_invalid(id, groups):
+    size = len(id) // groups
+    target = id[:size]
+    for i in range(size, len(id), size):
+        if id[i:i+size] != target:
+            return False
+    return True
 
 with open(argv[1]) as f:
     ranges = [[int(d) for d in p.split('-')] for p in [pair for pair in f.read().rstrip().split(',')]]
 
 part1 = 0
+part2 = 0
 for r in ranges:
     for id in range(r[0], r[1]+1):
         id_str = str(id)
-        if len(id_str) % 2 == 0 and is_invalid(id_str):
+        if len(id_str) % 2 == 0 and is_invalid(id_str, 2):
             part1 += id
 
+        for i in range(2, len(id_str) + 1):
+            if len(id_str) % i == 0 and is_invalid(id_str, i):
+                part2 += id
+                break
+
 print('Part 1:', part1)
+print('Part 2:', part2)
 
 
     
