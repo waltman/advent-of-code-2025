6f98c1f341edb4fcebf11162d2f7d6f65bc15573
Walt Mankowski <waltman@pobox.com>
2025-12-02 09:19:58 -0500

    use regexes to do the matching

--- day02/gift_shop.py
+++ day02/gift_shop.py
@@ -1,12 +1,5 @@
 from sys import argv
-
-def is_invalid(id, groups):
-    size = len(id) // groups
-    target = id[:size]
-    for i in range(size, len(id), size):
-        if id[i:i+size] != target:
-            return False
-    return True
+import re
 
 with open(argv[1]) as f:
     ranges = [[int(d) for d in p.split('-')] for p in [pair for pair in f.read().rstrip().split(',')]]
@@ -16,13 +9,11 @@ part2 = 0
 for r in ranges:
     for id in range(r[0], r[1]+1):
         id_str = str(id)
-        if len(id_str) % 2 == 0 and is_invalid(id_str, 2):
+        if re.fullmatch(r'(\d+)\1', id_str):
             part1 += id
 
-        for i in range(2, len(id_str) + 1):
-            if len(id_str) % i == 0 and is_invalid(id_str, i):
-                part2 += id
-                break
+        if re.fullmatch(r'(\d+)\1+', id_str):
+            part2 += id
 
 print('Part 1:', part1)
 print('Part 2:', part2)
