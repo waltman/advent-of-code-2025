6c6c473fdb863ff2866f0a8bfd66470fa5811db1
Walt Mankowski <waltman@pobox.com>
2025-12-02 11:03:33 -0500

    Sets are iterable so I don't need to make them lists to sum them

--- day02/gift_shop_sets.py
+++ day02/gift_shop_sets.py
@@ -19,8 +19,8 @@ dups |= {int(str(i) * copies) for i,copies in product(range(1,10), range(6,11))}
 part1 = 0
 part2 = 0
 for r in ranges:
-    part1 += sum(list(set(range(r[0], r[1]+1)) & pairs))
-    part2 += sum(list(set(range(r[0], r[1]+1)) & dups))
+    part1 += sum(set(range(r[0], r[1]+1)) & pairs)
+    part2 += sum(set(range(r[0], r[1]+1)) & dups)
 
 print('Part 1:', part1)
 print('Part 2:', part2)
