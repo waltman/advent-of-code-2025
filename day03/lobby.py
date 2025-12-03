d5f2c80a4f22ddcf2f29f7be0a443e812ee6afe5
Walt Mankowski <waltman@pobox.com>
2025-12-03 08:01:12 -0500

    added code to solve 3.2

--- day03/lobby.py
+++ day03/lobby.py
@@ -1,19 +1,29 @@
 from sys import argv
 from itertools import combinations
 
+def best_joltage(bank):
+    joltage = 0
+    pos = 0
+    for i in range(12):
+        remaining = 12-i
+        last_n = len(bank) - remaining + 1
+        best_val = 0
+        for n in range(pos, last_n):
+            if bank[n] > best_val:
+                best_val = bank[n]
+                best_pos = n
+            if best_val == 9:
+                break
+        joltage = joltage * 10 + best_val
+        pos = best_pos + 1
+
+    return joltage
+
 with open(argv[1]) as f:
     banks = [[int(c) for c in line.rstrip()] for line in f]
 
 part1 = sum([max([n1*10 + n2 for n1,n2 in combinations(bank, 2)]) for bank in banks])
 print('Part 1:', part1)
 
-part2 = 0
-for bank in banks:
-    bank2 = ''.join([str(n) for n in bank])
-    print(bank2)
-    best = 0
-    for comb in combinations(bank2, 12):
-        best = max(best, int(''.join(comb)))
-    part2 += best
-    
+part2 = sum([best_joltage(bank) for bank in banks])
 print('Part 2:', part2)
