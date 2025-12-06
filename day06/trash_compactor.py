a00be58708bdf8c4557276367535afe7c7ee4822
Walt Mankowski <waltman@pobox.com>
2025-12-06 06:26:38 -0500

    updated to solve 6.2

--- day06/trash_compactor.py
+++ day06/trash_compactor.py
@@ -1,13 +1,16 @@
 import sys
 import numpy as np
+from math import prod
 
 def main():
     rows = []
+    raw = []
     with open(sys.argv[1]) as f:
         for line in f:
             toks = line.split()
             if toks[0].isnumeric():
                 rows.append([int(tok) for tok in toks])
+                raw.append(line)
             else:
                 ops = toks
 
@@ -21,5 +24,30 @@ def main():
             part1 += np.prod(grid[:,col])
 
     print('Part 1:', part1)
+
+    # turn the raw grid into the values for part 2
+    ncols = len(raw[0]) - 1
+    vals = []
+    cols = []
+    for col in range(ncols):
+        val_str = ''
+        for row in range(len(raw)):
+            val_str += raw[row][col]
+        val_str = val_str.strip()
+        if val_str == '':
+            cols.append(vals.copy())
+            vals.clear()
+        else:
+            vals.append(int(val_str))
+    cols.append(vals.copy())
+
+    part2 = 0
+    for col in range(len(cols)):
+        if ops[col] == '+':
+            part2 += sum(cols[col])
+        else:
+            part2 += prod(cols[col])
+
+    print('Part 2:', part2)
     
 main()
